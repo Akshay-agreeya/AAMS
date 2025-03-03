b66a642867e0c0b869b276da9a4d9238c1b283d2
AgreeYa123 <abhishek.joshi@agreeya.com>
2025-03-03 13:40:28 +0530

    Loadingchanges

--- src/pages/UserManagement/UserManagement.jsx
+++ src/pages/UserManagement/UserManagement.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import editOrgicon from "../../assets/images/iconWhiteEdit.svg";
 import DeleteConfirmationModal from "../../component/dialog/DeleteConfirmation";
 import Layout from '../../component/Layout';
-import ChangePasswordModal from '../../common/auth/ChangePassword'
+import ChangePasswordModal from '../../common/auth/ChangePassword';
 import Accordian from "../../component/accordian/Accordian";
 import { deleteData, getData } from "../../utils/CommonApi";
 import { UserTable } from "./UserTable";
@@ -11,49 +11,53 @@ import notification from "../../component/notification/Notification";
 
 const UserManagement = () => {
   const [organizations, setOrganizations] = useState([]);
-
+  const [loading, setLoading] = useState(false); // Loading state added
   const navigate = useNavigate();
 
   useEffect(() => {
     getOrganizations();
-  }, [])
+  }, []);
 
   const getOrganizations = async () => {
+    
+  
     try {
+      setLoading(true);
       const resp = await getData("/org/list");
       setOrganizations(resp.data.organizations);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
-  }
+
+    finally{
+      setLoading(false);
+    }
+  };
+
   const handleDeleteOrganization = async () => {
     const selectedOrg = organizations.filter(item => item.selected);
     const data = {
-      org_ids:selectedOrg.map(item=>item.org_id)
-   }
+      org_ids: selectedOrg.map(item => item.org_id)
+    };
     try {
-      const resp = await deleteData(`/org/delete`,data);
+      const resp = await deleteData(`/org/delete`, data);
       notification.success({
         title: `Delete Organization`,
         message: resp.message
       });
       navigate(0);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
       notification.error({
         title: 'Delete Organization',
         message: error.data?.error
-      })
+      });
     }
-  }
+  };
 
   return (
     <Layout>
       <div className="adaMainContainer">
-        {/* Breadcrumb */}
-        {/* Admin Panel Content */}
         <section className="adminControlContainer">
           <div className="container">
             <div className="row">
@@ -61,7 +65,7 @@ const UserManagement = () => {
                 <div className="pageTitle">
                   <h1>User Management - Organization ({organizations.length})</h1>
                   <div className="buttonContainer">
-                    <a href="#" onClick={(e)=>{e.preventDefault()}} className="delete me-1" data-bs-toggle={organizations.filter(item => item.selected).length > 0 ? "modal" : ""} data-bs-target="#deleteUserModal">
+                    <a href="#" onClick={(e) => { e.preventDefault() }} className="delete me-1" data-bs-toggle={organizations.filter(item => item.selected).length > 0 ? "modal" : ""} data-bs-target="#deleteUserModal">
                       <i className="fa-regular fa-trash-can"></i> Delete
                     </a>
                     <a href="/admin/user-management/addorg" className="add">
@@ -71,37 +75,47 @@ const UserManagement = () => {
                 </div>
               </div>
 
-              {/* Accordion Section */}
-              <div className="col-12">
-                <div className="userManagmentContainer">
-                  <div className="accordion" id="userManageList">
-                    {organizations.map((org) => (
-                      <Accordian title={org.org_name} prefix={<div className="form-check me-2 custCheck">
-                        <input className="form-check-input" type="checkbox" id={`addcheck-${org.org_id}`}
-                          value="Add" onChange={(e) => { org.selected = e.target.checked; setOrganizations([...organizations]) }} />
-                      </div>} extra={<div className="addNewUserCont">
-                        <a href={`/admin/user-management/editorganization/${org.org_id}`} className="edit me-1">
-                          <img src={editOrgicon} alt="Edit Organization" /> Edit Organization
-                        </a>
-                        <a href={`/admin/user-management/adduser/${org.org_id}`} className="add">
-                          <i className="fa-solid fa-plus"></i> Add New User
-                        </a>
-                      </div>}>
-                        <UserTable org_id={org.org_id}/>
-                      </Accordian>
-                    ))}
+              {/* Show loading indicator for 3 seconds */}
+              {loading ? (
+                <div className="dataLoadContainer">
+                  <div className="progressBarContainer">
+                    <div className="message">Loading data, please wait...</div>
+                    <div className="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                      <div className="progress-bar progress-bar-striped progress-bar-animated" style={{ width: "55%" }}></div>
+                    </div>
                   </div>
-                  {/* Accordion Ends */}
-                  {/* Delete Confirmation Modal - Reusable Component */}
-                  <DeleteConfirmationModal modalId="deleteUserModal" onDelete={handleDeleteOrganization}/>
-                  <ChangePasswordModal id="changePassword" />
                 </div>
-              </div>
+              ) : (
+                <div className="col-12">
+                  <div className="userManagmentContainer">
+                    <div className="accordion" id="userManageList">
+                      {organizations.map((org) => (
+                        <Accordian title={org.org_name} prefix={<div className="form-check me-2 custCheck">
+                          <input className="form-check-input" type="checkbox" id={`addcheck-${org.org_id}`}
+                            value="Add" onChange={(e) => { org.selected = e.target.checked; setOrganizations([...organizations]) }} />
+                        </div>} extra={<div className="addNewUserCont">
+                          <a href={`/admin/user-management/editorganization/${org.org_id}`} className="edit me-1">
+                            <img src={editOrgicon} alt="Edit Organization" /> Edit Organization
+                          </a>
+                          <a href={`/admin/user-management/adduser/${org.org_id}`} className="add">
+                            <i className="fa-solid fa-plus"></i> Add New User
+                          </a>
+                        </div>}>
+                          <UserTable org_id={org.org_id} />
+                        </Accordian>
+                      ))}
+                    </div>
+
+                    <DeleteConfirmationModal modalId="deleteUserModal" onDelete={handleDeleteOrganization} />
+                    <ChangePasswordModal id="changePassword" />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </section>
-      </div >
-    </Layout >
+      </div>
+    </Layout>
   );
 };
 
